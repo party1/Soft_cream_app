e619812ecea340329978a300b3be908fe0a1da9c
siroa <blueskymizo1218@outlook.jp>
2017-12-11 14:41:39 +0900

    通信機能搭載

---
+++ soft_cream_app/soft_cream_appDlg.h
@@ -0,0 +1,46 @@
+
+// soft_cream_appDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+#include "afxwin.h"
+
+
+// Csoft_cream_appDlg ダイアログ
+class Csoft_cream_appDlg : public CDialogEx
+{
+// コンストラクション
+public:
+	Csoft_cream_appDlg(CWnd* pParent = NULL);	// 標準コンストラクター
+
+// ダイアログ データ
+	enum { IDD = IDD_SOFT_CREAM_APP_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnDestroy();
+
+private:
+	CStatic m_glView;
+	CDC *m_pDC;
+	HGLRC m_GLRC;
+	BOOL SetUpPixelFormat(HDC hdc);
+public:
+	CEdit msgED;
+	LRESULT OnMessageRCV(WPARAM wParam, LPARAM lParam);
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+};
